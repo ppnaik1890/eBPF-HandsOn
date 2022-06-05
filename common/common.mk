529bc0d73e67967194a63d19ee0efaddf11851d4
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2022-06-05 16:59:14 +0530

    Clean file

--- common/common.mk
+++ common/common.mk
@@ -23,55 +23,37 @@ USER_OBJ := ${USER_C:.c=.o}
 COMMON_DIR ?= ../common/
 
 # Extend if including Makefile already added some
-COMMON_OBJS += $(COMMON_DIR)/common_params.o $(COMMON_DIR)/common_user_bpf_xdp.o
+COMMON_OBJS += $(COMMON_DIR)/common_user_bpf_xdp.o $(COMMON_DIR)/common_params.o
 
 # Create expansions for dependencies
 COMMON_H := ${COMMON_OBJS:.o=.h}
 
-# CFLAGS ?= -I$(LIBBPF_DIR)/build/usr/include/ -g
-# CFLAGS += -I../headers/
-# LDFLAGS ?= -L$(LIBBPF_DIR)
-
 LIBS = -lbpf -lelf $(USER_LIBS)
 
-all: llvm-check $(USER_TARGETS) $(XDP_OBJ)
+all: $(USER_TARGETS) $(XDP_OBJ)
 
 .PHONY: clean $(CLANG) $(LLC)
 
 clean:
-	rm -f $(USER_TARGETS) $(XDP_OBJ) $(USER_OBJ) $(COPY_LOADER) $(COPY_STATS)
+	rm -f $(USER_TARGETS) $(XDP_OBJ) $(USER_OBJ)
 	rm -f *.ll
 	rm -f *~
 
 # For build dependency on this file, if it gets updated
 COMMON_MK = $(COMMON_DIR)/common.mk
 
-llvm-check: $(CLANG) $(LLC)
-	@for TOOL in $^ ; do \
-		if [ ! $$(command -v $${TOOL} 2>/dev/null) ]; then \
-			echo "*** ERROR: Cannot find tool $${TOOL}" ;\
-			exit 1; \
-		else true; fi; \
-	done
-
-# Create dependency: detect if C-file change and touch H-file, to trigger
-# target $(COMMON_OBJS)
-$(COMMON_H): %.h: %.c
-	touch $@
-
 # Detect if any of common obj changed and create dependency on .h-files
 $(COMMON_OBJS): %.o: %.h
 	make -C $(COMMON_DIR)
 
-$(USER_TARGETS): %: %.c  $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS) $(KERN_USER_H) $(EXTRA_DEPS)
-	$(CC) -Wall $(CFLAGS) $(LDFLAGS) -o $@ $(COMMON_OBJS) \
+$(USER_TARGETS): %: %.c  Makefile $(COMMON_MK) $(COMMON_OBJS)
+	$(CC) -Wall -o $@ $(COMMON_OBJS) \
 	 $< $(LIBS)
 
-$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK) $(KERN_USER_H) $(EXTRA_DEPS) $(OBJECT_LIBBPF)
+$(XDP_OBJ): %.o: %.c  Makefile $(COMMON_MK)
 	$(CLANG) -S \
 	    -target bpf \
 	    -D __BPF_TRACING__ \
-	    $(BPF_CFLAGS) \
 	    -Wall \
 	    -Wno-unused-value \
 	    -Wno-pointer-sign \
