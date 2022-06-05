62bfe68c8f6e5d7ead670f799392a4defd2f28d2
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2022-06-05 19:41:18 +0530

    Clean common_user_bpf_xdp.h

--- common/common_user_bpf_xdp.c
+++ common/common_user_bpf_xdp.c
@@ -1,19 +1,11 @@
-#include <bpf/libbpf.h> /* bpf_get_link_xdp_id + bpf_set_link_xdp_id */
-#include <string.h>     /* strerror */
-#include <net/if.h>     /* IF_NAMESIZE */
-#include <stdlib.h>     /* exit(3) */
-#include <errno.h>
-
 #include <bpf/bpf.h>
 #include <bpf/libbpf.h>
-
+#include <errno.h>
 #include <linux/if_link.h> /* Need XDP flags */
-
 
 #include "common_defines.h"
 
-int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd)
-{
+int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd) {
 	int err;
 
 	/* libbpf provide the XDP net_device link-level hook attach helper */
@@ -27,28 +19,31 @@ int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd)
 		__u32 old_flags = xdp_flags;
 
 		xdp_flags &= ~XDP_FLAGS_MODES;
-		xdp_flags |= (old_flags & XDP_FLAGS_SKB_MODE) ? XDP_FLAGS_DRV_MODE : XDP_FLAGS_SKB_MODE;
+		xdp_flags |= (old_flags & XDP_FLAGS_SKB_MODE) ? XDP_FLAGS_DRV_MODE
+													  : XDP_FLAGS_SKB_MODE;
 		err = bpf_set_link_xdp_fd(ifindex, -1, xdp_flags);
-		if (!err)
-			err = bpf_set_link_xdp_fd(ifindex, prog_fd, old_flags);
+		if (!err) err = bpf_set_link_xdp_fd(ifindex, prog_fd, old_flags);
 	}
 	if (err < 0) {
-		fprintf(stderr, "ERR: "
-			"ifindex(%d) link set xdp fd failed (%d): %s\n",
-			ifindex, -err, strerror(-err));
+		fprintf(stderr,
+				"ERR: "
+				"ifindex(%d) link set xdp fd failed (%d): %s\n",
+				ifindex, -err, strerror(-err));
 
 		switch (-err) {
-		case EBUSY:
-		case EEXIST:
-			fprintf(stderr, "Hint: XDP already loaded on device"
-				" use --force to swap/replace\n");
-			break;
-		case EOPNOTSUPP:
-			fprintf(stderr, "Hint: Native-XDP not supported"
-				" use --skb-mode or --auto-mode\n");
-			break;
-		default:
-			break;
+			case EBUSY:
+			case EEXIST:
+				fprintf(stderr,
+						"Hint: XDP already loaded on device"
+						" use --force to swap/replace\n");
+				break;
+			case EOPNOTSUPP:
+				fprintf(stderr,
+						"Hint: Native-XDP not supported"
+						" use --skb-mode or --auto-mode\n");
+				break;
+			default:
+				break;
 		}
 		return EXIT_FAIL_XDP;
 	}
@@ -56,41 +51,48 @@ int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd)
 	return EXIT_OK;
 }
 
-int xdp_link_detach(int ifindex, __u32 xdp_flags, __u32 expected_prog_id)
-{
+int xdp_link_detach(int ifindex, __u32 xdp_flags, __u32 expected_prog_id) {
 	__u32 curr_prog_id;
 	int err;
 
 	err = bpf_get_link_xdp_id(ifindex, &curr_prog_id, xdp_flags);
 	if (err) {
-		fprintf(stderr, "ERR: get link xdp id failed (err=%d): %s\n",
-			-err, strerror(-err));
+		fprintf(stderr, "ERR: get link xdp id failed (err=%d): %s\n", -err,
+				strerror(-err));
 		return EXIT_FAIL_XDP;
 	}
 
 	if (!curr_prog_id) {
-		printf("INFO: %s() no curr XDP prog on ifindex:%d\n",
-			    __func__, ifindex);
+		printf("INFO: %s() no curr XDP prog on ifindex:%d\n", __func__,
+			   ifindex);
 		return EXIT_OK;
 	}
 
 	if (expected_prog_id && curr_prog_id != expected_prog_id) {
-		fprintf(stderr, "ERR: %s() "
-			"expected prog ID(%d) no match(%d), not removing\n",
-			__func__, expected_prog_id, curr_prog_id);
+		fprintf(stderr,
+				"ERR: %s() "
+				"expected prog ID(%d) no match(%d), not removing\n",
+				__func__, expected_prog_id, curr_prog_id);
 		return EXIT_FAIL;
 	}
 
 	if ((err = bpf_set_link_xdp_fd(ifindex, -1, xdp_flags)) < 0) {
 		fprintf(stderr, "ERR: %s() link set xdp failed (err=%d): %s\n",
-			__func__, err, strerror(-err));
+				__func__, err, strerror(-err));
 		return EXIT_FAIL_XDP;
 	}
 
-	
-	printf("INFO: %s() removed XDP prog ID:%d on ifindex:%d\n",
-		    __func__, curr_prog_id, ifindex);
+	printf("INFO: %s() removed XDP prog ID:%d on ifindex:%d\n", __func__,
+		   curr_prog_id, ifindex);
 
 	return EXIT_OK;
 }
 
+int init_map_fd(struct bpf_object* obj, const char* map_name) {
+	int fd = bpf_object__find_map_fd_by_name(obj, map_name);
+	if (fd < 0) {
+		fprintf(stderr, "Failed to find map %s\n", map_name);
+		return -ENOENT;
+	}
+	return fd;
+}
--- common/common_user_bpf_xdp.h
+++ common/common_user_bpf_xdp.h
@@ -6,5 +6,6 @@
 
 int xdp_link_attach(int ifindex, __u32 xdp_flags, int prog_fd);
 int xdp_link_detach(int ifindex, __u32 xdp_flags, __u32 expected_prog_id);
+int init_map_fd(struct bpf_object* obj, const char* map_name);
 
 #endif /* __COMMON_USER_BPF_XDP_H */
