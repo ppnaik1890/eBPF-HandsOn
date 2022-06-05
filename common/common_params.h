efbf7efa797a5f599d73197d1149b2e30e6efa92
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2022-06-05 16:43:57 +0530

    Add simplfied command line parsing functions

--- common/common_params.h
+++ common/common_params.h
@@ -2,21 +2,11 @@
 #ifndef __COMMON_PARAMS_H
 #define __COMMON_PARAMS_H
 
-#include <getopt.h>
 #include "common_defines.h"
 
-struct option_wrapper {
-  struct option option;
-  char *help;
-  char *metavar;
-  bool required;
-};
 
-void usage(const char *prog_name, const char *doc,
-           const struct option_wrapper *long_options, bool full);
+void usage(const char *prog_name);
 
-void parse_cmdline_args(int argc, char **argv,
-			const struct option_wrapper *long_options,
-                        struct config *cfg, const char *doc);
+void parse_cmdline_args(int argc, char **argv,struct arguments *args);
 
 #endif /* __COMMON_PARAMS_H */
